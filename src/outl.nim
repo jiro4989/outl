d42bb1373f028e27cc478bec2e18d24b8209acea
jiro4989 <jiroron666@gmail.com>
2021-01-19 09:18:22 +0900

    add formatInt

--- src/outl.nim
+++ src/outl.nim
@@ -1,9 +1,14 @@
-import stats, algorithm
+import stats, algorithm, math
+from strutils import align, alignLeft
 
 type
   AggregateResult = object
     count: int
     min, max, sum, average, median, percentile95: float64
+  RoundType = enum
+    roundOff, ceil, floor
+  PaddingType = enum
+    right, left
 
 proc percentile(datas: openArray[float64], percent: int): float64 =
     ## datas はソート済みでなければならない。
@@ -30,6 +35,20 @@ proc aggregate(data: seq[float64], needMedian = false, needPercentile95 = false)
     result.median = data.percentile(50)
   if needPercentile95:
     result.percentile95 = data.percentile(95)
+
+proc formatInt(data: float64, roundType = roundOff, paddingType = right, padChar = ' ', paddingDigit = 0): string =
+  let num =
+    case roundType
+    of roundOff: data.round
+    of ceil: data.ceil
+    of floor: data.floor
+
+  result = $num
+  if 0 < paddingDigit:
+    result =
+      case paddingType
+      of right: result.align(paddingDigit, padChar)
+      of left: result.alignLeft(paddingDigit, padChar)
 
 when isMainModule and not defined modeTest:
   discard
